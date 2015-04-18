9333c33819a0601b7af419cf3212b1f9ecb343b3
lbohl <lbohl@expedia.com>
2015-04-18 15:17:27 +0100

    update javadoc

--- parsed-args/src/main/java/com/github/methylene/args/ParseResult.java
+++ parsed-args/src/main/java/com/github/methylene/args/ParseResult.java
@@ -18,22 +18,44 @@ public class ParseResult {
   }
 
   static ParseResult failure(List<String> reasons) {
-    assert reasons != null && reasons.size() > 0;
+    if (reasons == null || reasons.size() == 0)
+      throw new IllegalStateException("failure without messages, this should never happen");
     return new ParseResult(null, reasons);
   }
 
+  /**
+   * Check if parsing was successful.
+   * If this method returns true, {@link #getMessages} will return an empty list and
+   * {@link #get} will not return null.
+   * @return true if parsing was successful
+   */
   public boolean isSuccess() {
     return parsedArgs != null;
   }
 
+  /**
+   * Check if parsing was successful.
+   * If this method returns true, {@link #getMessages} will return a non-empty list and
+   * {@link #get} will return null.
+   * @return true if parsing was successful
+   */
   public boolean isFailure() {
     return !isSuccess();
   }
 
+  /**
+   * Get the list of parsing errors. If this method returns an empty list,
+   * then there were no parsing errors, and {@link #isSuccess} will return {@code true}.
+   * @return
+   */
   public List<String> getMessages() {
     return messages;
   }
 
+  /**
+   * Get the parsing result or {@code null} if there was an error.
+   * @return the parsed arguments or {@code null} if  parsing failed
+   */
   public ParsedArgs get() {
     return parsedArgs;
   }
